22350a65fd0a334ddc8a45e6f0d09340619091f4
Colin Cassell <col.cassell@gatech.edu>
2026-02-22 18:51:03 -0500

    Content updates

--- apps/personal/app/page.tsx
+++ apps/personal/app/page.tsx
@@ -8,7 +8,7 @@ export default function ChillinOutPage() {
         <body className={`antialiased bg-white dark:bg-black text-black dark:text-white`}>
             <main
                 className="flex flex-col items-center justify-center min-h-screen py-2 bg-repeat"
-                style={{ backgroundImage: "url('/fun/turtwig.png')" }}
+                style={{ backgroundImage: "url('/fun/turtwig.png')", backgroundSize: "150px auto", height: "100vh" }}
             >
                 <h1 className="text-4xl font-bold">Chillin' Out</h1>
                 <p className="mt-4 text-lg">We out here.</p>
