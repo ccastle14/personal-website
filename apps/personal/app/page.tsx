ff8f0a956c89ed3b451704743fd89cc67764d446
Colin Cassell <col.cassell@gatech.edu>
2026-02-26 22:06:48 -0500

    Adds the noid

--- apps/personal/app/page.tsx
+++ apps/personal/app/page.tsx
@@ -8,20 +8,30 @@ export default function ChillinOutPage() {
       className="flex flex-col items-center justify-center min-h-screen py-2 bg-personal"
       style={{ backgroundImage: "url('/fun/turtwig.png')", backgroundSize: "150px auto", height: "100vh" }}
     >
-      <div className="background flex flex-col">
-        <h1 className="retro-title">Welcome to Turtwig Town</h1>
-        <p className="retro-paragraph">A very serious website dedicated to our #1 lil guy</p>
-        <div className="mt-auto flex">
+      <div className="flex flex-row  gap-8">
+        <div>
           <img
-            src="fun/bus_meme.jpg"
-            className="w-full max-w-[250px] h-auto"
-            alt="turtwig bus meme"
+            src="fun/the-noid.png"
+            className="max-w-[50px] h-auto"
+            alt="it is the noid."
           />
-          <img
-            src="fun/yell_meme.jpg"
-            className="w-full max-w-[350px] h-auto ml-auto"
-            alt="turtwig yell meme"
-          />
+          RESPECT THE NOID
+        </div>
+        <div className="background flex flex-col">
+          <h1 className="retro-title">Welcome to Turtwig Town</h1>
+          <p className="retro-paragraph">A very serious website dedicated to our #1 lil guy</p>
+          <div className="mt-auto flex">
+            <img
+              src="fun/bus_meme.jpg"
+              className="w-full max-w-[250px] h-auto"
+              alt="turtwig bus meme"
+            />
+            <img
+              src="fun/yell_meme.jpg"
+              className="w-full max-w-[350px] h-auto ml-auto"
+              alt="turtwig yell meme"
+            />
+          </div>
         </div>
       </div>
     </main>
